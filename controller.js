26f1ee26dc9cd2d0529eab4d446423dd51fdd17b
Charles Lehnert <charles@cllinteractive.com>
2021-08-29 15:21:08 -0400

    refactor: Remove use of 'moment' package and clean up logging

--- controller.js
+++ controller.js
@@ -7,10 +7,12 @@ require("log-timestamp");
 
 const mqtt = require('mqtt');
 const Api = require('./api');
-const moment = process.env.NODE_ENV === 'development' && require('moment');
 
 const readableTime = timestamp => {
-  return moment(timestamp).format('D MMM YYYY h:mm:ss A');
+  const newDate = new Date();
+  newDate.setTime(timestamp);
+  const dateString = newDate.toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'long' });
+  return dateString;
 }
 
 const cameraStartTimeByMac = {};
@@ -48,16 +50,18 @@ client.on('connect', () => {
   client.publish('unifi/protect-downloader/availability', 'online', { qos: 1, retain: true });
 
   if (cameraNames.length > 0) {
-    console.info(`Subcribing to motion events for cameras: ${cameraNames.join(', ')}`);
+    console.info('[controller] Subscribing to motion events for cameras: %s', cameraNames.join(', '));
     cameraNames.map(cameraName => client.subscribe(`unifi/camera/motion/${cameraName}`));
   } else {
-    console.info('Subcribing to motion events for all cameras');
+    console.info('[controller] Subscribing to motion events for all cameras');
     client.subscribe('unifi/camera/motion/#');
   }
 });
 
 client.on('message', (topic, message) => {
-
+  if (process.env.VERBOSE == true && process.env.NODE_ENV === 'development') {
+    console.info('[controller] Received message for topic: %s: %s', topic, message);
+  }
   if (topic.startsWith('unifi/camera/motion/')) {
     const splitMessage = topic.split('unifi/camera/motion/')[1].split('/');
     const cameraName = splitMessage[0];
@@ -67,12 +71,12 @@ client.on('message', (topic, message) => {
       return processMotionEvent({ isMotionDetected, cameraName, timestamp });
     }
   }
-  console.warn('[controller] No handler for topic: %s', topic);
+  console.warn('[controller] No handler for topic: %s: %s', topic, message);
 });
 
 const processMotionEvent = async ({ isMotionDetected, cameraName, timestamp }) => {
   if (process.env.VERBOSE == true && process.env.NODE_ENV === 'development') {
-    console.info(`[controller] Processing motion event with status: ${isMotionDetected}`);
+    console.info('[controller] Processing motion event with status: %s', isMotionDetected);
   }
   if (isMotionDetected === '1') {
     console.info('[controller] Processing motion start event');
@@ -83,7 +87,7 @@ const processMotionEvent = async ({ isMotionDetected, cameraName, timestamp }) =
     } else if (!cameraStartTimeByMac[cameraName]) {
       cameraStartTimeByMac[cameraName] = timestamp;
       if (process.env.NODE_ENV === 'development') {
-        console.info(`[controller] Set start time: ${readableTime(timestamp)}`);
+        console.info('[controller] Set start time: %s', readableTime(timestamp));
       }
     }
 
@@ -97,9 +101,9 @@ const processMotionEvent = async ({ isMotionDetected, cameraName, timestamp }) =
       // timeout to see if new movement is started
       cameraDownloadQueue[cameraName] = setTimeout(() => {
         const delay = process.env.DOWNLOAD_DELAY || 5000;
-        console.info(`[controller] Motion end event finished; processing video download after ${delay/1000} seconds`);
+        console.info('[controller] Motion end event finished; processing video download after %s seconds', delay/1000);
         if (process.env.NODE_ENV === 'development') {
-          console.info(`[controller] Do download for start time: ${readableTime(startTimestamp)}`);
+          console.info('[controller] Do download for start time: %s', readableTime(startTimestamp));
         }
         delete cameraStartTimeByMac[cameraName];
         delete cameraDownloadQueue[cameraName];
